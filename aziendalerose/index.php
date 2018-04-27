8b5eb61b8fc131c6dfe21ac152916d78de087994
Daniele A <35625213+dam72ge@users.noreply.github.com>
2018-04-27 16:00:44 +0200

    Add files via upload

---
+++ aziendalerose/index.php
@@ -0,0 +1,22 @@
+<?php
+$url="";
+
+$title="Homepage";
+$metaDescription="Sito internet dell'Azienda agricola Le Rose di Noli Fabrizio";
+$metaKeywords="azienda agricola le rose noli fabrizio serra riccò genova frutta ortaggi di stagione carne";
+$metaRobots="ALL";
+
+include "inc/head.php";
+?>
+<p>
+La nostra azienda prende il nome dalla passione di coltivare le rose di nonna Gina, fondatrice dell'azienda circa sessant'anni fa.<br/><br/>
+E' un'azienda a conduzione familiare dove lavorano Fabrizio, Gabriella, Dario e Walter.<br/><br/>
+Produciamo frutta e ortaggi di stagione in particolare i fagiolini lambarden, patate e anche le patate quarantine.<br/><br/>
+In azienda inoltre vendiamo polli, conigli, ecc.<br/>
+Concimiamo il tutto con il letame dei nostri bovini che poi vengono macellati e confezionati in pacchi da 5/10 kg.<br/><br/>
+VI ASPETTIAMO IN AZIENDA! VENITE A VEDERE CON I VOSTRI OCCHI CIO' CHE COMPRATE!<br/><br/>
+A PRESTO!</p>
+
+<?php
+include "inc/footer.php";
+?>
